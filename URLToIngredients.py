e10d8f8e13508db5d333686da5e6264027abc73f
crypwalk <65584405+crypwalk@users.noreply.github.com>
2020-06-16 12:00:47 -0600

    proprietary Ingredient-Lists.py introduced

---
+++ URLToIngredients.py
@@ -0,0 +1,20 @@
+import requests
+from bs4 import BeautifulSoup
+
+def UrlToIngredients(botLim, topLim, URL):
+    listList = []
+    for i in range(botLim, topLim):
+        ingredientList = []
+        try:
+            page = requests.get(URL)
+            soup = BeautifulSoup(page.content, 'html.parser')
+        except:
+            print('recipe doesn\'t exist')
+            break
+
+        ingredients = soup.find_all(class_="ingredient-description")
+        for ingredient in ingredients:
+            if ingredient.text not in ingredientList:
+                ingredientList.append(ingredient.text)
+        listList.append(ingredientList)
+    return listList
